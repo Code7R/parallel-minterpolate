eb10605fb742bf98b41084517a04bc26c7ffa825
Eduard Bloch <blade@debian.org>
2025-11-03 00:46:54 +0100

    Simplify overcomplicated use of argparse result

--- parallel-minterpolate.py
+++ parallel-minterpolate.py
@@ -31,45 +31,45 @@ parser.add_argument('--shutdown',
                     action='store_true',
                     help='shutdown computer after tasks are completed')
 
-args = vars(parser.parse_args())
+args = parser.parse_args()
 
 # create video capture object
-videoData = cv2.VideoCapture(args['inputVideo'].name)
+videoData = cv2.VideoCapture(args.inputVideo.name)
 # count the number of frames
 frames = videoData.get(cv2.CAP_PROP_FRAME_COUNT)
 fps = videoData.get(cv2.CAP_PROP_FPS)
 videoSeconds = round(frames / fps)
-# calculate duration of the splitted parts
-partsSeconds = round(videoSeconds / args['split'])
+# calculate duration of the split parts
+partsSeconds = round(videoSeconds / args.split)
 partsTime = datetime.timedelta(seconds=partsSeconds)
 
 # create output dir if not exists
-if os.path.isdir(args['o, __outputDir']) == False:
-    os.makedirs(args['o, __outputDir'])
+if not os.path.isdir(args.outputDir):
+    os.makedirs(args.outputDir)
 
 # create and write the input text file for ffmpeg concat
-f = open(f"{args['o, __outputDir']}/list.txt", 'x')
-for i in range(args['split']):
-    f.write(f"file 'output{str(i).zfill(3)}.{args['fps']}fps.mp4'\n")
+f = open(f"{args.outputDir}/list.txt", 'x')
+for i in range(args.split):
+    f.write(f"file 'output{str(i).zfill(3)}.{args.fps}fps.mp4'\n")
 f.close()
 
 # write batch file
-f = open(f"{args['o, __outputDir']}/run.bat", 'x')
+f = open(f"{args.outputDir}/run.bat", 'x')
 f.write(
-    f"ffmpeg -i \".{args['inputVideo'].name}\" -c copy -map 0 -segment_time {partsTime} -f segment -reset_timestamps 1 output%%03d.mp4\n")
+    f"ffmpeg -i \".{args.inputVideo.name}\" -c copy -map 0 -segment_time {partsTime} -f segment -reset_timestamps 1 output%%03d.mp4\n")
 
 # launch all ffmpeg tasks in parallel
 # continue only when everything is finished
 f.write('(\n')
-for i in range(args['split']):
+for i in range(args.split):
     f.write(
-        f"  start \"TASK {i+1}\" ffmpeg -i output{str(i).zfill(3)}.mp4 -crf 10 -vf \"minterpolate=fps={args['fps']}:mi_mode=mci:mc_mode=aobmc:me_mode=bidir:vsbmc=1\" output{str(i).zfill(3)}.{args['fps']}fps.mp4\n")
+        f"  start \"TASK {i+1}\" ffmpeg -i output{str(i).zfill(3)}.mp4 -crf 10 -vf \"minterpolate=fps={args.fps}:mi_mode=mci:mc_mode=aobmc:me_mode=bidir:vsbmc=1\" output{str(i).zfill(3)}.{args.fps}fps.mp4\n")
 f.write(') | pause\n')
 
 f.write('timeout /t 3 /nobreak > nul\n')
 f.write('ffmpeg -f concat -safe 0 -i list.txt -c copy final.mp4\n')
 
-if args['shutdown']:
+if args.shutdown:
     f.write('timeout /t 3 /nobreak > nul\n')
     f.write('shutdown /s /f /t 0\n')
 f.close()
