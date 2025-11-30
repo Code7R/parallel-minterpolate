006a6a0e6ea7be922fd179696e9fe388bee5f753
Eduard Bloch <blade@debian.org>
2025-11-30 10:07:43 +0100

    Configurable ffmpeg command

--- parallel-minterpolate.py
+++ parallel-minterpolate.py
@@ -7,6 +7,15 @@ import os
 import subprocess
 import json
 
+ffmpeg = "ffmpeg -y"
+
+try:
+    result = subprocess.run(["bash", "--version"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
+    use_bash = bool(result) and ("GNU bash, " in result.stdout)
+    ffmpeg = "nice -n18 " + ffmpeg
+except:
+    use_bash = False
+
 parser = argparse.ArgumentParser(
     description='Parallelize video frame interpolation with FFmpeg.')
 parser.add_argument('inputVideo',
@@ -34,13 +43,19 @@ parser.add_argument('--shutdown',
 parser.add_argument('--autoname', "-A",
                     action='store_true',
                     help="Choose a name based in the input's filename (default: use final.mkv as filename)")
+parser.add_argument('-F, --ffmpeg-cmd',
+                    metavar='CMD',
+                    default=ffmpeg,
+                    dest="ffmpeg",
+                    help=f'ffmpeg command to use (default: "{ffmpeg}")')
 args = parser.parse_args()
 
-probeResult = subprocess.run(["ffprobe", "-v", "quiet", "-print_format", "json", "-show_format", "-show_streams", "-print_format", "json", args.inputVideo.name], stdout=subprocess.PIPE, text=True)
+probeCmd = ["ffprobe", "-v", "quiet", "-print_format", "json", "-show_format", "-show_streams", "-print_format", "json", args.inputVideo.name]
+#print(" ".join(probeCmd))
+probeResult = subprocess.run(probeCmd, stdout=subprocess.PIPE, text=True)
 probed = json.loads(probeResult.stdout)
 #print(repr(probed))
 
-ffmpeg = "ffmpeg -y"
 
 odir = os.path.abspath(os.path.normpath(args.outputDir))
 os.makedirs(odir, exist_ok=True)
@@ -77,25 +92,14 @@ for i in range(args.split):
     f.write(f"file 'output{str(i).zfill(3)}.{args.fps}fps.mkv'\n")
 f.close()
 
-use_bash = False
-
 if args.autoname:
     oname_stem, oname_ext = os.path.splitext(os.path.basename(args.inputVideo.name))
     oname = f"{oname_stem}.{args.fps}fps{oname_ext}"
 else:
     oname = "final.mkv"
 
-try:
-    result = subprocess.run(["bash", "--version"], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
-    use_bash = bool(result) and ("GNU bash, " in result.stdout)
-except:
-    use_bash = False
+script = "run.sh" if use_bash else "run.bat"
 
-if use_bash:
-    script = "run.sh"
-    ffmpeg = "nice -n18 " + ffmpeg
-else:
-    script = "run.bat"
 script_abs = os.path.join(odir, script)
 
 # write batch file
@@ -107,7 +111,7 @@ if use_bash:
 f.write(f"cd \"{odir}\"\n")
 
 f.write(
-    f"{ffmpeg} -i \"{ifile}\" -c copy {map_video} {map_audio} -segment_time {partsTime} -f segment -reset_timestamps 1 output%03d.mkv\n")
+    f"{args.ffmpeg} -i \"{ifile}\" -c copy {map_video} {map_audio} -segment_time {partsTime} -f segment -reset_timestamps 1 output%03d.mkv\n")
 
 # launch all ffmpeg tasks in parallel
 # continue only when everything is finished
@@ -123,7 +127,7 @@ for i in range(args.split):
         fork_pfx =  f"  start \"TASK {i+1}\" "
         fork_sfx = ""
     f.write(
-        f"  {fork_pfx} {ffmpeg} -i output{str(i).zfill(3)}.mkv {map_video} {map_audio} -crf 10 -vf \"minterpolate=fps={args.fps}:mi_mode=mci:mc_mode=aobmc:me_mode=bidir:vsbmc=1\" output{str(i).zfill(3)}.{args.fps}fps.mkv{fork_sfx}\n")
+        f"  {fork_pfx} {args.ffmpeg} -i output{str(i).zfill(3)}.mkv {map_video} {map_audio} -crf 10 -vf \"minterpolate=fps={args.fps}:mi_mode=mci:mc_mode=aobmc:me_mode=bidir:vsbmc=1\" output{str(i).zfill(3)}.{args.fps}fps.mkv{fork_sfx}\n")
 
 if use_bash:
     f.writelines(map(lambda i: f"wait $ff_pid_{i};\n", range(args.split)))
@@ -133,7 +137,7 @@ else:
 if not use_bash:
     f.write('timeout /t 3 /nobreak > nul\n')
 
-f.write(f'{ffmpeg} -f concat -safe 0 -i list.txt {input_subs} -c copy {map_video} {map_audio} {map_subs} \"{oname}\"\n')
+f.write(f'{args.ffmpeg} -f concat -safe 0 -i list.txt {input_subs} -c copy {map_video} {map_audio} {map_subs} \"{oname}\"\n')
 
 if args.shutdown and not use_bash:
     f.write('timeout /t 3 /nobreak > nul\n')
